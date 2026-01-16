8e5bda49095a9667a913d0549e1019c7203894ad
Luis Alonso Alvarez Aguilar <l.alvarezaguilar448@gmail.com>
2026-01-16 13:35:48 -0500

    [ADD] Se agrega método que devuelve los roles de un usuario autenticado

--- src/main/java/com/pilli3800/inventario/service/UserService.java
+++ src/main/java/com/pilli3800/inventario/service/UserService.java
@@ -3,8 +3,13 @@ package com.pilli3800.inventario.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -20,5 +25,20 @@ public class UserService {
         }
 
         return auth.getName();
+    }
+
+    public List<String> getUserRoles() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null) {
+            return authentication.getAuthorities().stream()
+                    .map(GrantedAuthority::getAuthority) // Extraemos el nombre del rol
+                    .collect(Collectors.toList());
+        }
+        return Collections.emptyList(); // Si no hay autenticación, devolvemos una lista vacía
+    }
+
+    public boolean isAdmin() {
+        List<String> roles = getUserRoles();
+        return roles.contains("ROLE_ADMINISTRACION");
     }
 }
