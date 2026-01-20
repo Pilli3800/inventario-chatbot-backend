8cb47f1c3f3a0fdde5b8c96ef995890e5d5e281c
Luis Alonso Alvarez Aguilar <l.alvarezaguilar448@gmail.com>
2026-01-20 15:16:38 -0500

    [UPD] Se agrega auditoria a la entidad Role

--- src/main/java/com/pilli3800/inventario/data/models/user/Role.java
+++ src/main/java/com/pilli3800/inventario/data/models/user/Role.java
@@ -1,18 +1,19 @@
 package com.pilli3800.inventario.data.models.user;
 
+import com.pilli3800.inventario.data.models.auditoria.RegistroAuditoria;
 import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
 @Table(name = "roles")
 @Data
-public class Role {
+public class Role extends RegistroAuditoria {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    // ADMINISTRACIÓN, LOGÍSTICA, GERENCIA, USUARIO, JEFE_CUADRILLA
+    // ADMINISTRACION, LOGISTICA, GERENCIA, JEFE_CUADRILLA
     @Column(name = "nombre", nullable = false, unique = true, length = 50)
     private String nombreRol;
 
